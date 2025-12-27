9904cf283d4a5b9e41d72ee1c2b9dc0937073855
Roshin P Raj <roshinpraj2001@gmail.com>
2025-12-27 12:31:12 +0530

    Fix Django project structure and settings for deployment

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,10 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'                   # URL prefix for browser
-STATIC_ROOT = BASE_DIR / 'staticfiles'    # Folder on disk (your existing folder name)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_URL = '/staticfiles/'                   # URL prefix for browser
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # Directory to collect static files
 
 
 MEDIA_URL='/images/'
